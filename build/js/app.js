c82ad1cb946d170ba9632f21fed6295c2dd296ab
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-06 15:31:33 -0700

    install all dependences, add all gulp.tasks - UI and backend works

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,7 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "e24653d1d0658cc2fc909c144fbc9043";
+
+},{}],2:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
 
 function Doctor(){
@@ -30,3 +34,17 @@ Doctor.prototype.getDoctors = function(medicalIssue, doctorData) {
 };
 
 exports.doctorModule = Doctor;
+
+},{"./../.env":1}],3:[function(require,module,exports){
+var Doctor = require('./../js/doctors.js').doctorModule;
+
+$(document).ready(function() {
+  $('#doctor-form').submit(function(event) {
+    event.preventDefault();
+    var medicalIssue = $('#medicalissue').val();
+    var doctorData = new Doctor();
+    doctorData.getDoctors(medicalIssue, doctorData);
+  });
+});
+
+},{"./../js/doctors.js":2}]},{},[3]);
--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,46 +1,10 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
-
-var displayDoctor = function(medicalIssue, doctorData) {
-  $('#showDoctors').text("Doctors for  " + medicalIssue + " are: " + doctorData);
-}
-
-
+var Doctor = require('./../js/doctors.js').doctorModule;
 
 $(document).ready(function() {
-  var doctorObject = new Weather();
-
-$('#medicalissue').click(function() {
+  $('#doctor-form').submit(function(event) {
+    event.preventDefault();
     var medicalIssue = $('#medicalissue').val();
-    $('medicalissue').val("");
-    doctorObject.getDoctor(medicalIssue, displayDoctor);
+    var doctorData = new Doctor();
+    doctorData.getDoctors(medicalIssue, doctorData);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-//
-// var displayDoctorsArray = function(medicalIssue, doctorsData) {
-//   $('#showDoctors').text("List of Doctors in Portland: "  + " " + convertToArray(doctorsData));
-// };
-//
-// var convertToArray = function(doctors) {
-//   var doctorsArray = doctors.split();
-//   return doctorsArray;
-// };
-//
-// $(document).ready(function() {
-//   $('#issues').click(function() {
-//       var currentDoctorObject = new Doctor();
-//       var medicalIssue = $('#medicalissue').val();
-//       $('#medicalissue').val("");
-//       currentDoctorObject.convertToArray(medicalIssue, displayDoctorsArray);
-//     });
-// });
